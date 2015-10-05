ceeae3501ea6bb7c13b4fe6cd19a43b2753621db
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2015-10-05 11:35:30 -0300

    Added methods to format a string using the defined mask

--- library/src/androidTest/java/com/luckcheese/maskedTextWatcher/MaskedTextWatcherTester.java
+++ library/src/androidTest/java/com/luckcheese/maskedTextWatcher/MaskedTextWatcherTester.java
@@ -7,15 +7,15 @@ import android.widget.EditText;
 public class MaskedTextWatcherTester extends AndroidTestCase {
 
     private EditText editText;
-    private MaskedTextWatcher watcher;
+    private MaskedTextWatcher mask;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        watcher = new MaskedTextWatcher(null);
+        mask = new MaskedTextWatcher(null);
 
         editText = new EditText(getContext());
-        editText.addTextChangedListener(watcher);
+        editText.addTextChangedListener(mask);
         editText.requestFocus();
     }
 
@@ -33,6 +33,50 @@ public class MaskedTextWatcherTester extends AndroidTestCase {
 
         editText.setSelection(1, 3);
         keyboardKeyPressed(KeyEvent.KEYCODE_G, "agm");
+    }
+
+    public void testCleanUpString() throws Exception {
+        mask.setMask("##");
+        assertEquals("a", mask.clean("a"));
+        assertEquals("aa", mask.clean("aa"));
+        assertEquals("ab", mask.clean("ab"));
+
+        mask.setMask("# #");
+        assertEquals("a", mask.clean("a"));
+        assertEquals("aa", mask.clean("a a"));
+        assertEquals("ab", mask.clean("a b"));
+        assertEquals("ac", mask.clean("acb"));
+        assertEquals("ac", mask.clean("ac "));
+
+        mask.setMask(" # #");
+        assertEquals("a", mask.clean(" a"));
+        assertEquals("aa", mask.clean(" a a"));
+        assertEquals("ab", mask.clean(" a b"));
+        assertEquals("#b", mask.clean(" # b"));
+    }
+
+    public void testFormat() throws Exception {
+        mask.setMask("##");
+        assertEquals("a", mask.format("a"));
+        assertEquals("aa", mask.format("aa"));
+        assertEquals("ab", mask.format("ab"));
+
+        mask.setMask("# #");
+        assertEquals("a ", mask.format("a"));
+        assertEquals("a a", mask.format("aa"));
+        assertEquals("a b", mask.format("ab"));
+
+        mask.setMask(" #-#");
+        assertEquals(" a-", mask.format("a"));
+        assertEquals(" a-a", mask.format("aa"));
+        assertEquals(" a-b", mask.format("ab"));
+
+        mask.setMask("# #/#-##00");
+        assertEquals("0 ", mask.format("0"));
+        assertEquals("0 1/", mask.format("01"));
+        assertEquals("0 2/1-", mask.format("021"));
+        assertEquals("0 2/1-3", mask.format("0213"));
+        assertEquals("0 2/1-3400", mask.format("02134"));
     }
 
     // ----- Helper methods ---------------------------------------------------
--- library/src/main/java/com/luckcheese/maskedTextWatcher/MaskedTextWatcher.java
+++ library/src/main/java/com/luckcheese/maskedTextWatcher/MaskedTextWatcher.java
@@ -6,27 +6,77 @@ import android.text.TextWatcher;
 public class MaskedTextWatcher implements TextWatcher {
 
     private String mask;
+    private int variableCharsQty;
 
     public MaskedTextWatcher(String mask) {
         this.mask = mask;
+        setMask(mask);
     }
 
     public void setMask(String mask) {
-        this.mask = mask;
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+        if (mask == null || mask.length() == 0) {
+            this.mask = null;
+        }
+        else {
+            this.mask = mask;
+            this.variableCharsQty = 0;
+            for (int i = 0; i < mask.length(); i++) {
+                if (mask.charAt(i) == '#') {
+                    variableCharsQty++;
+                }
+            }
+        }
     }
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (mask == null) return;
 
     }
 
     @Override
-    public void afterTextChanged(Editable s) {
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // do nothing
+    }
 
+    @Override
+    public void afterTextChanged(Editable s) {
+        // do nothing
+    }
+
+    protected String applyMaskToString(String originalString) {
+        if (mask == null) return originalString;
+        return format(clean(originalString));
+    }
+
+    protected String clean(String editTextString) {
+        StringBuilder cleaned = new StringBuilder();
+        for (int i = 0; i < editTextString.length(); i++) {
+            char c = editTextString.charAt(i);
+            if (c != mask.charAt(i) || c == '#') {
+                cleaned.append(c);
+            }
+
+            if (cleaned.length() == variableCharsQty) {
+                break;
+            }
+        }
+        return cleaned.toString();
+    }
+
+    protected String format(String cleanedString) {
+        StringBuilder formatted = new StringBuilder();
+        for (int maskIndex = 0, stringIndex = 0; maskIndex < mask.length(); maskIndex++) {
+            char c = mask.charAt(maskIndex);
+            if (c == '#') {
+                if (stringIndex >= cleanedString.length()) break;
+                formatted.append(cleanedString.charAt(stringIndex));
+                stringIndex++;
+            }
+            else {
+                formatted.append(c);
+            }
+        }
+        return formatted.toString();
     }
 }
