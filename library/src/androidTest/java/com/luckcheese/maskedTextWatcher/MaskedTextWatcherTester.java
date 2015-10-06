e05be92c262fb0a71a6c5926bc015c31a276f86b
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2015-10-06 15:06:40 -0300

    Improved tests to also check cursor pos after each change

--- library/src/androidTest/java/com/luckcheese/maskedTextWatcher/MaskedTextWatcherTester.java
+++ library/src/androidTest/java/com/luckcheese/maskedTextWatcher/MaskedTextWatcherTester.java
@@ -21,19 +21,19 @@ public class MaskedTextWatcherTester extends AndroidTestCase {
     }
 
     public void testNullMask() throws Exception {
-        keyboardKeyPressed(KeyEvent.KEYCODE_A, "a");
-        keyboardKeyPressed(KeyEvent.KEYCODE_M, "am");
-        keyboardKeyPressed(KeyEvent.KEYCODE_DEL, "a");
-        keyboardKeyPressed(KeyEvent.KEYCODE_N, "an");
+        keyboardKeyPressed(KeyEvent.KEYCODE_A, "a", 1);
+        keyboardKeyPressed(KeyEvent.KEYCODE_M, "am", 2);
+        keyboardKeyPressed(KeyEvent.KEYCODE_DEL, "a", 1);
+        keyboardKeyPressed(KeyEvent.KEYCODE_N, "an", 2);
 
         editText.setSelection(1);
-        keyboardKeyPressed(KeyEvent.KEYCODE_M, "amn");
+        keyboardKeyPressed(KeyEvent.KEYCODE_M, "amn", 2);
 
         editText.setSelection(3);
-        keyboardKeyPressed(KeyEvent.KEYCODE_M, "amnm");
+        keyboardKeyPressed(KeyEvent.KEYCODE_M, "amnm", 4);
 
         editText.setSelection(1, 3);
-        keyboardKeyPressed(KeyEvent.KEYCODE_G, "agm");
+        keyboardKeyPressed(KeyEvent.KEYCODE_G, "agm", 2);
     }
 
     public void testCleanUpString() throws Exception {
@@ -111,48 +111,48 @@ public class MaskedTextWatcherTester extends AndroidTestCase {
 
     public void testCardNumberMask() throws Exception {
         mask.setMask("#### #### #### ####");
-        keyboardKeyPressed(KeyEvent.KEYCODE_0, "0");
-        keyboardKeyPressed(KeyEvent.KEYCODE_1, "01");
-        keyboardKeyPressed(KeyEvent.KEYCODE_2, "012");
-        keyboardKeyPressed(KeyEvent.KEYCODE_3, "0123 ");
-        keyboardKeyPressed(KeyEvent.KEYCODE_4, "0123 4");
-        keyboardKeyPressed(KeyEvent.KEYCODE_5, "0123 45");
-        keyboardKeyPressed(KeyEvent.KEYCODE_6, "0123 456");
-        keyboardKeyPressed(KeyEvent.KEYCODE_7, "0123 4567 ");
-        keyboardKeyPressed(KeyEvent.KEYCODE_8, "0123 4567 8");
-        keyboardKeyPressed(KeyEvent.KEYCODE_9, "0123 4567 89");
-        keyboardKeyPressed(KeyEvent.KEYCODE_A, "0123 4567 89a");
-        keyboardKeyPressed(KeyEvent.KEYCODE_B, "0123 4567 89ab ");
-        keyboardKeyPressed(KeyEvent.KEYCODE_C, "0123 4567 89ab c");
-        keyboardKeyPressed(KeyEvent.KEYCODE_D, "0123 4567 89ab cd");
-        keyboardKeyPressed(KeyEvent.KEYCODE_E, "0123 4567 89ab cde");
-        keyboardKeyPressed(KeyEvent.KEYCODE_F, "0123 4567 89ab cdef");
-        keyboardKeyPressed(KeyEvent.KEYCODE_0, "0123 4567 89ab cdef");
-        keyboardKeyPressed(KeyEvent.KEYCODE_1, "0123 4567 89ab cdef");
+        keyboardKeyPressed(KeyEvent.KEYCODE_0, "0", 1);
+        keyboardKeyPressed(KeyEvent.KEYCODE_1, "01", 2);
+        keyboardKeyPressed(KeyEvent.KEYCODE_2, "012", 3);
+        keyboardKeyPressed(KeyEvent.KEYCODE_3, "0123 ", 5);
+        keyboardKeyPressed(KeyEvent.KEYCODE_4, "0123 4", 6);
+        keyboardKeyPressed(KeyEvent.KEYCODE_5, "0123 45", 7);
+        keyboardKeyPressed(KeyEvent.KEYCODE_6, "0123 456", 8);
+        keyboardKeyPressed(KeyEvent.KEYCODE_7, "0123 4567 ", 10);
+        keyboardKeyPressed(KeyEvent.KEYCODE_8, "0123 4567 8", 11);
+        keyboardKeyPressed(KeyEvent.KEYCODE_9, "0123 4567 89", 12);
+        keyboardKeyPressed(KeyEvent.KEYCODE_A, "0123 4567 89a", 13);
+        keyboardKeyPressed(KeyEvent.KEYCODE_B, "0123 4567 89ab ", 15);
+        keyboardKeyPressed(KeyEvent.KEYCODE_C, "0123 4567 89ab c", 16);
+        keyboardKeyPressed(KeyEvent.KEYCODE_D, "0123 4567 89ab cd", 17);
+        keyboardKeyPressed(KeyEvent.KEYCODE_E, "0123 4567 89ab cde", 18);
+        keyboardKeyPressed(KeyEvent.KEYCODE_F, "0123 4567 89ab cdef", 19);
+        keyboardKeyPressed(KeyEvent.KEYCODE_0, "0123 4567 89ab cdef", 19);
+        keyboardKeyPressed(KeyEvent.KEYCODE_1, "0123 4567 89ab cdef", 19);
     }
 
     public void testDateMask() throws Exception {
         mask.setMask("##/##/####");
-        keyboardKeyPressed(KeyEvent.KEYCODE_0, "0");
-        keyboardKeyPressed(KeyEvent.KEYCODE_1, "01/");
-        keyboardKeyPressed(KeyEvent.KEYCODE_2, "01/2");
-        keyboardKeyPressed(KeyEvent.KEYCODE_3, "01/23/");
-        keyboardKeyPressed(KeyEvent.KEYCODE_4, "01/23/4");
-        keyboardKeyPressed(KeyEvent.KEYCODE_5, "01/23/45");
-        keyboardKeyPressed(KeyEvent.KEYCODE_6, "01/23/456");
-        keyboardKeyPressed(KeyEvent.KEYCODE_7, "01/23/4567");
+        keyboardKeyPressed(KeyEvent.KEYCODE_0, "0", 1);
+        keyboardKeyPressed(KeyEvent.KEYCODE_1, "01/", 3);
+        keyboardKeyPressed(KeyEvent.KEYCODE_2, "01/2", 4);
+        keyboardKeyPressed(KeyEvent.KEYCODE_3, "01/23/", 6);
+        keyboardKeyPressed(KeyEvent.KEYCODE_4, "01/23/4", 7);
+        keyboardKeyPressed(KeyEvent.KEYCODE_5, "01/23/45", 8);
+        keyboardKeyPressed(KeyEvent.KEYCODE_6, "01/23/456", 9);
+        keyboardKeyPressed(KeyEvent.KEYCODE_7, "01/23/4567", 10);
     }
 
     public void testMaskChangingSelection() throws Exception {
         mask.setMask("# #/#-##");
-        keyboardKeyPressed(KeyEvent.KEYCODE_0, "0 ");
-        keyboardKeyPressed(KeyEvent.KEYCODE_1, "0 1/");
+        keyboardKeyPressed(KeyEvent.KEYCODE_0, "0 ", 2);
+        keyboardKeyPressed(KeyEvent.KEYCODE_1, "0 1/", 4);
         editText.setSelection(2);
-        keyboardKeyPressed(KeyEvent.KEYCODE_2, "0 2/1-");
-        keyboardKeyPressed(KeyEvent.KEYCODE_3, "0 2/3-1");
-        keyboardKeyPressed(KeyEvent.KEYCODE_4, "0 2/3-41");
-        keyboardKeyPressed(KeyEvent.KEYCODE_5, "0 2/3-45");
-        keyboardKeyPressed(KeyEvent.KEYCODE_6, "0 2/3-45");
+        keyboardKeyPressed(KeyEvent.KEYCODE_2, "0 2/1-", 4);
+        keyboardKeyPressed(KeyEvent.KEYCODE_3, "0 2/3-1", 6);
+        keyboardKeyPressed(KeyEvent.KEYCODE_4, "0 2/3-41", 7);
+        keyboardKeyPressed(KeyEvent.KEYCODE_5, "0 2/3-45", 8);
+        keyboardKeyPressed(KeyEvent.KEYCODE_6, "0 2/3-45", 8);
     }
 
     public void testPastText() throws Exception {
@@ -186,8 +186,9 @@ public class MaskedTextWatcherTester extends AndroidTestCase {
 
     // ----- Helper methods ---------------------------------------------------
 
-    private void keyboardKeyPressed(int keyCode, String expectedResult) {
+    private void keyboardKeyPressed(int keyCode, String expectedResult, int expectedCursorPor) {
         editText.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, keyCode));
         assertEquals(expectedResult, editText.getText().toString());
+        assertEquals(expectedCursorPor, editText.getSelectionStart());
     }
 }
