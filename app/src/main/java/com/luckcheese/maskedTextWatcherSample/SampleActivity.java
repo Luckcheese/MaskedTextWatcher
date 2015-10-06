eb40f87bc1984cc0a78bbb49ca92e6f9c658368f
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2015-10-06 15:52:27 -0300

    Improved sample app so user can choose the keyboard type

--- app/src/main/java/com/luckcheese/maskedTextWatcherSample/SampleActivity.java
+++ app/src/main/java/com/luckcheese/maskedTextWatcherSample/SampleActivity.java
@@ -2,8 +2,10 @@ package com.luckcheese.maskedTextWatcherSample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import com.luckcheese.maskedTextWatcher.MaskedTextWatcher;
 
@@ -12,6 +14,7 @@ public class SampleActivity extends Activity implements View.OnClickListener {
 
     private EditText maskField;
     private EditText testField;
+    private RadioGroup radioGroup;
 
     private MaskedTextWatcher maskedTextWatcher;
 
@@ -21,6 +24,7 @@ public class SampleActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_sample);
 
         maskField = (EditText) findViewById(R.id.mask);
+        radioGroup = (RadioGroup) findViewById(R.id.keyboardStyle);
         testField = (EditText) findViewById(R.id.testArea);
 
         findViewById(R.id.apply).setOnClickListener(this);
@@ -33,6 +37,15 @@ public class SampleActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.numbers:
+                testField.setInputType(InputType.TYPE_CLASS_NUMBER);
+                break;
+
+            case R.id.text:
+                testField.setInputType(InputType.TYPE_CLASS_TEXT);
+                break;
+        }
         maskedTextWatcher.setMask(maskField.getText().toString());
     }
 }
